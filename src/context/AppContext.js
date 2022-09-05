67c8f3b14d44d5bb0d32770a939349fab87dcf53
Andrez Ramirez <andrezzz2312@gmail.com>
2022-09-05 11:48:17 -0500

    asd

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,12 +3,12 @@ import { createContext, useState, useEffect } from 'react'
 export const AppContext = createContext()
 
 const AppContextProvider = ({ children }) => {
-	const [ language, setLanguage ] = useState('es')
+	const [language, setLanguage] = useState('es')
 
 	useEffect(() => {
 		const systemLanguage = window.navigator.language.split('-')[0]
 
-		if(systemLanguage === 'es') {
+		if (systemLanguage === 'es') {
 			setLanguage('es')
 		} else {
 			setLanguage('en')
@@ -17,7 +17,7 @@ const AppContextProvider = ({ children }) => {
 
 	return (
 		<AppContext.Provider value={{ language, setLanguage }}>
-			{ children }
+			{children}
 		</AppContext.Provider>
 	)
 }
